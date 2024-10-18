1e0699553e174b742be4582d3b1fd8bed8d1e9f0
rakshaji <rakshasinghania@gmail.com>
2024-10-18 15:50:05 +0530

    reorganization of rojects, added input output files and folders, modified java to add head child nodes like scripts and links through code

---
+++ js/aanchalFashionCustomScript.js
@@ -0,0 +1,5 @@
+ $(function() {
+                  //$('#tinyurl a').miniPreview({ prefetch: 'pageload' });
+                  $('#tinyurl a').miniPreview({ prefetch: 'parenthover' });
+                  //$('#p3 a').miniPreview({ prefetch: 'none' });
+              });
